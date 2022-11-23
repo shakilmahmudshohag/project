20d78d3d10772453d5111bd62d686da225e9ebd6
shakilmahmudshohag <shakil15-13588@diu.edu.bd>
2022-11-23 22:31:02 +0600

    commit

---
+++ lib/practice.dart
@@ -0,0 +1,21 @@
+//constants
+//const age = 27;
+//const twiceTheAge = age * 2;
+
+//variables
+//var name = "kilmu";//final name ="kilmu"
+//name ="moonlight";
+
+//Functions
+//String getFullName(String firstName, String lastName) {
+ // return firstName + '' + lastName; //return '$firstName $lastname';
+//}
+//below widget build
+//print(getFullName('kilmu','Moonlight'));
+
+//String printMyName() {
+//  return '';
+//}
+
+//control statement and collections
+
